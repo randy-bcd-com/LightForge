02d9df930cebdf9da084bd47a2bc232aaab4a7af
Randy Lathrop <Randy.Lathrop@A-dec.com>
2020-11-16 23:31:52 -0800

    add .gitigmore

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(void)
 	/* Initializes MCU, drivers and middleware */
 	atmel_start_init();
 
-	/* Replace with your application code */
+	/* This is my application code !!*/
 	while (1) {
           gpio_set_pin_level(LED0, true);
           gpio_set_pin_level(LED0, false);
