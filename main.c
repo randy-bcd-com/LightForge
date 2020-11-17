4f0474523029f827513b5e6f0b91a515b2fd7520
Randy Lathrop <Randy.Lathrop@A-dec.com>
2020-11-16 23:29:51 -0800

    Initial Commit. blinks LED0

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <atmel_start.h>
+
+int main(void)
+{
+	/* Initializes MCU, drivers and middleware */
+	atmel_start_init();
+
+	/* Replace with your application code */
+	while (1) {
+          gpio_set_pin_level(LED0, true);
+          gpio_set_pin_level(LED0, false);
+	}
+}
